f3448a46ce3b694bacb00d4374d4a78b557d002e
Jeronimo Vallelunga <jeronimo.vallelunga@reflektion.com>
2019-03-17 13:24:17 -0300

    Updates

---
+++ precache-manifest.1d02825bb6b4d21342fe313208da20c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1be946493c49419459e3",
+    "url": "/redux-bluetooth/static/js/runtime~main.62cbf256.js"
+  },
+  {
+    "revision": "da0b350246c672ad3e64",
+    "url": "/redux-bluetooth/static/js/main.7df7c763.chunk.js"
+  },
+  {
+    "revision": "53cc343935e499d42616",
+    "url": "/redux-bluetooth/static/js/2.2698f9e4.chunk.js"
+  },
+  {
+    "revision": "da0b350246c672ad3e64",
+    "url": "/redux-bluetooth/static/css/main.66a8e5c7.chunk.css"
+  },
+  {
+    "revision": "b8b40ed685649df082bbc556700c9b32",
+    "url": "/redux-bluetooth/index.html"
+  }
+];
